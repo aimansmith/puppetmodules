a0df175f52dd89a53c1dc62e292acab0284301d6
Naton Aiman-Smith <naton@amazon.com>
2014-04-25 12:11:38 -0400

    Got rid of ssh git pull

--- demoapp/manifests/init.pp
+++ demoapp/manifests/init.pp
@@ -6,23 +6,9 @@ class demoapp {
   package {"perl-Time-HiRes": ensure => installed, }->
   service {"httpd": ensure => running, }->
 
-  # Deploy the app via git
-  # Set up ssh
-  file {"/root/.ssh/config":
-	ensure => present,
-	content => "HostName github.com
-  StrictHostKeyChecking no
-",
-	mode => 0600,
-  }->
-  # Should be doing this with a role but I'm too lazy to edit my CFN template
-  exec {"/usr/bin/aws s3 cp s3://naton-demos-west/celgene-event/demoapp/git-ssh-key /root/.ssh/id_rsa":
-	creates => "/root/.ssh/id_rsa",
-  }->
-  exec {"/bin/chmod 600 /root/.ssh/id_rsa": }->
   # Install git and clone
   package {"git": ensure => installed, }->
-  exec {"/usr/bin/git clone git@github.com:aimansmith/thoughts.git":
+  exec {"/usr/bin/git clone https://github.com/aimansmith/puppetmodules.git":
 	cwd => "/opt/src",
 	creates => "/opt/src/thoughts",
 	require => File["/opt/src"],
