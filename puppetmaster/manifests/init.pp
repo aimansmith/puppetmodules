86e2d770b60b82ea835316af69d34da5f286eb76
root <root@ip-10-50-2-41.us-west-2.compute.internal>
2014-04-25 16:19:17 +0000

    Added puppetmaster module

---
+++ puppetmaster/manifests/init.pp
@@ -0,0 +1,16 @@
+class puppetmaster {
+  # Set up ssh
+  file {"/root/.ssh/config":
+        ensure => present,
+        content => "HostName github.com
+  StrictHostKeyChecking no
+",
+        mode => 0600,
+  }->
+  # Should be doing this with a role but I'm too lazy to edit my CFN template
+  exec {"/usr/bin/aws s3 cp s3://naton-demos-west/celgene-event/puppetmaster/git-ssh-key /root/.ssh/id_rsa":
+        creates => "/root/.ssh/id_rsa",
+  }->
+  exec {"/bin/chmod 600 /root/.ssh/id_rsa": }->
+  package {"git": ensure => installed, }
+}
