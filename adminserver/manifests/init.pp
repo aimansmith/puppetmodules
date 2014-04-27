77a7d45fce1256b2a69a6bba42601b4270da26ab
root <root@ip-10-50-2-41.us-west-2.compute.internal>
2014-04-27 13:20:10 +0000

    adminserver fixes

--- adminserver/manifests/init.pp
+++ adminserver/manifests/init.pp
@@ -1,3 +1,9 @@
 class adminserver {
-  package {"mysql":  ensure => installed, }
+  package {"mysql":  ensure => installed, }->
+  file {"/usr/local/bin/backup_demoapp_db.sh":
+	mode => 0755,
+	content => template('adminserver/backup_demoapp_db.sh.erb'),
+	replace => yes,
+	ensure => present,
+  }
 }
