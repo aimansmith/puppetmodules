a1c3b9550560f7d129b794b63a0da5a8bd4bfd67
Nate Aiman-Smith <aimansmith@gmail.com>
2014-04-27 19:59:07 +0000

    working on adding in tag support

--- demoapp/manifests/init.pp
+++ demoapp/manifests/init.pp
@@ -6,13 +6,33 @@ class demoapp {
   package {"perl-Time-HiRes": ensure => installed, }->
   service {"httpd": ensure => running, }->
 
-  # Install git and clone
+  # Install git
   package {"git": ensure => installed, }->
-  exec {"/usr/bin/git clone https://github.com/aimansmith/thoughts":
+  # Figure out desired branch / tag
+  # Branch takes precendence
+  case $demoapp_branch {
+	"master": {
+	  # This means we have no branch, look for a tag
+	  case $demoapp_tag {
+	    "none": { notify {"no tag given, using HEAD": } }
+	    /(\d|\w)/: {
+		# We have something
+		$git_branchtag="$demoapp_tag"
+	    }
+	    default: { notify {"no tag given, using HEAD": } $git_branchtag="master" }
+	  }
+	}
+	default: {
+	  # This should match pretty much anything
+	  $git_branchtag="$demoapp_branch"
+	}
+  }->
+  exec {"/usr/bin/git clone -b $git_branchtag https://github.com/aimansmith/thoughts":
 	cwd => "/opt/src",
 	creates => "/opt/src/thoughts",
 	require => File["/opt/src"],
   }->
+
   # Install the app
   exec {"/opt/src/thoughts/install.sh": }->
   file {"/opt/src/thoughts.properties":
